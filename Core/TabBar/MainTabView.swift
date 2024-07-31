244dc76955036a6a6a2c58be9dd4944fc78699b9
aydinnkaya <Aydinnkaya1@gmail.com>
2024-07-31 09:44:24 +0300

    mainTabView
    mainTabView Created

---
+++ Core/TabBar/MainTabView.swift
@@ -0,0 +1,40 @@
+//
+//  MainTabView.swift
+//  SnapScene
+//
+//  Created by Aydın KAYA on 31.07.2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView{
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            Text("Notifactions")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        
+    }
+}
+
+#Preview {
+    MainTabView()
+}
