13436411eebbf7557eb4c6aec024a14c97f426f8
aydinnkaya <Aydinnkaya1@gmail.com>
2024-08-10 12:22:25 +0300

    CreatedFeedView
    Created FeedView

---
+++ Core/Feed/FeedCellView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCellView.swift
+//  SnapScene
+//
+//  Created by Aydın KAYA on 7.08.2024.
+//
+
+import SwiftUI
+
+struct FeedCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCellView()
+}
